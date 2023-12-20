eb8458867447038cb1e874ea64ae9688d66d4ed8
Gaurav9354761050 <154440576+Gaurav9354761050@users.noreply.github.com>
2023-12-20 19:38:46 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,88 @@
+<?php error_reporting(0); $name = $_GET['name']; 
+
+
+$page_name = $name." Special surprise gift"; 
+
+
+
+
+$finger = "1"; 
+
+$text_finger1 = "You have a special surprise gift";
+$text_finger2 = "Scan your finger to view your surprising gift.";
+
+
+
+
+
+$sharing_url = "https://website.com";
+
+$sharing_message = $name." You have a special surprise gift Here"; 
+
+$sharing_invite = "Click here to view your big surprising gift.";
+
+$sharing_advice = "Just a big surprise gift here!";
+
+
+
+
+
+
+
+$sharing_name = "Click here to view your big surprising gift."; 
+
+$sharing_description = "Just a big surprise gift here!";
+
+
+
+
+
+
+$happy_from = "HAPPY BIRTHDAY";
+
+$happy_message = "All the best wishes";
+
+
+
+
+
+$aid_creat = "You can create your own greeting message by entering the name in the box bellow and clicking the Create button";
+
+
+
+
+/***   google adsense validation ID    ***/
+$ads_validation = "ca-pub-XXXXXXXXXXXXXXXX";
+
+
+/***   data ad client small   ***/
+$small_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
+                
+/***   data ad solt small   ***/
+$small_ads_slot = "XXXXXXXXXX";
+          
+
+
+
+/***   data ad client big    ***/
+$big_ads_id = "ca-pub-XXXXXXXXXXXXXXXX";
+
+/***   data ad solt big    ***/
+$big_ads_slot = "XXXXXXXXXX";
+
+
+
+
+/***   analytics ID    ***/
+$tracking = "UA-XXXXXXXXX-1";
+
+
+
+
+
+
+$fb_app_id = "XXXXXXXXXXXXXXXX";
+
+
+
+?>
